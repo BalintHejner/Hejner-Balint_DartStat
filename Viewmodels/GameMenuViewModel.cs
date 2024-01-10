304c089806f5ef8ca085fdce1d1ea9a674952755
BalintHejner <balinthejner204@gmail.com>
2024-01-10 14:27:56 +0100

    This is shite (again)

--- Viewmodels/GameMenuViewModel.cs
+++ Viewmodels/GameMenuViewModel.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Microsoft.Maui.Controls;
-using Hejner_Balint_DartStat;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using Hejner_Balint_DartStat.Views;
-using System.Security.Cryptography;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
 
 namespace Hejner_Balint_DartStat.Viewmodels
@@ -18,21 +7,25 @@ namespace Hejner_Balint_DartStat.Viewmodels
     public partial class GameMenuViewModel : ObservableObject
     {
         DartStatDatabase database;
-        public ICommand AppendDigitCommand { get; }
-        public ICommand ClearDisplayCommand { get; }
-        public ICommand SaveDisplayCommand { get; }
+        //public ICommand AppendDigitCommand { get; }
+        //public ICommand ClearDisplayCommand { get; }
+        //public ICommand SaveDisplayCommand { get; }
         public ObservableCollection<string> Scores { get; }
+        public int Score { get; set; }
+        public string Out { get; set; }
         public Dictionary<int, string> Outs { get; }
 
         private Entry displayEntry;
         private Label score_LB;
 
+        List<GameMenuViewModel> outts = new List<GameMenuViewModel>();
+
         [ObservableProperty] int legNumber;
 
-        public void EntryToWork(Entry displayEntry)
-        {
-            this.displayEntry = displayEntry;
-        }
+        //public void EntryToWork(Entry displayEntry)
+        //{
+        //    this.displayEntry = displayEntry;
+        //}
 
         public GameMenuViewModel()
         {
@@ -41,44 +34,59 @@ namespace Hejner_Balint_DartStat.Viewmodels
             {
                 Scores.Add(i.ToString());
             }
-            Outs = new Dictionary<int, string>();
-
-            //foreach (var item in File.ReadAllLines("Outs.txt"))
-            //{
-
-            //}
-            AppendDigitCommand = new Command<string>(AppendDigit);
-            ClearDisplayCommand = new Command(ClearDisplay);
-            SaveDisplayCommand = new Command(SaveDisplay);
+            //AppendDigitCommand = new Command<string>(AppendDigit);
+            //ClearDisplayCommand = new Command(ClearDisplay);
+            //SaveDisplayCommand = new Command(SaveDisplay);
             database = new DartStatDatabase();
+
+            // foreach (var item in File.ReadAllLines("Outs.txt"))
+            //{
+            //    GameMenuViewModel datas = new GameMenuViewModel(item);
+            //    outts.Add(datas);
+            //}
+
+
+            //foreach (var line in outts)
+            //{
+            //    if (!Outs.ContainsKey(line.Score))
+            //    {
+            //        Outs.Add(Score, Out);
+            //    }
+            //}
         }
 
-        private void AppendDigit(string digit)
-        {
-            displayEntry.Text += digit;
-        }
 
-        private void ClearDisplay()
-        {
-            displayEntry.Text = "";
-        }
+        //public GameMenuViewModel(string sor)
+        //{
+        //    string[] data = sor.Split(':');
+        //    Score = int.Parse(data[0]);
+        //    Out = data[1];
+        //}
 
-        private async void SaveDisplay()
-        {
-            int legs = 0;
-            for (int i = 1; i < LegNumber; i++)
-            {
-                legs = i;
-            }
+        //private void AppendDigit(string digit)
+        //{
+        //    displayEntry.Text += digit;
+        //}
 
-            Round ujKor = new Round(legs, legNumber*3, int.Parse(displayEntry.Text), int.Parse(score_LB.Text)  - int.Parse(displayEntry.Text), int.Parse(displayEntry.Text) / legNumber * 3);
+        //private void ClearDisplay()
+        //{
+        //    displayEntry.Text = "";
+        //}
 
-            displayEntry.Text = "";
+        //private async void SaveDisplay()
+        //{
+        //    int legs = 0;
+        //    for (int i = 1; i < LegNumber; i++)
+        //    {
+        //        legs = i;
+        //    }
 
-            await database.SaveItemAsync(ujKor);
-        }
+        //    Round ujKor = new Round(legs, legNumber*3, int.Parse(displayEntry.Text), int.Parse(score_LB.Text)  - int.Parse(displayEntry.Text), int.Parse(displayEntry.Text) / legNumber * 3);
 
-       
+        //    displayEntry.Text = "";
+
+        //    await database.SaveItemAsync(ujKor);
+        //}
 
     }
 }
--- Views/GameMenu.xaml.cs
+++ Views/GameMenu.xaml.cs
@@ -15,16 +15,32 @@ public partial class GameMenu : ContentPage
 		score_LB.Text = "501";
 	}
 
-	private void Load()
-	{
-		viewModel.EntryToWork(this.displayEntry);
-	}
+    //Bind the mainpage button text to score_LB.Text
+    //Bind the combobox selected item to legnumber label
+    //Fill the outs dictionary and bind it with the out_LB
+    //Make the saveBtn push data into SQLite and change the text of score_LB and out_LB
+    //Make the Toplist and Statistics pull down data from the database
 
-    protected override void OnAppearing()
+    private void Savebtn_Clicked(object sender, EventArgs e)
     {
-        base.OnAppearing();
-		Load();
+
     }
+
+    private void Clearbtn_Clicked(object sender, EventArgs e)
+    {
+        disappointingEntry.Text = "";
+    }
+
+    //private void Load()
+    //{
+    //	viewModel.EntryToWork(this.displayEntry);
+    //}
+
+    //   protected override void OnAppearing()
+    //   {
+    //       base.OnAppearing();
+    //	Load();
+    //   }
 
 
 }
